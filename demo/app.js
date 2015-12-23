954f50e57ee4b7aff28839c3c73f40fd34fa3648
Gerald <gera2ld@163.com>
2015-12-23 16:50:59 +0800

    Initial commit

---
+++ demo/app.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import TapasEditor from '../lib/tapas-editor';
+
+const config = {
+  statusbar: false,
+  resize: false,
+  menubar: '',
+  toolbar: 'undo redo | bold removeformat link image searchreplace',
+  plugins: 'searchreplace autoresize hg_image',
+  content_style:
+    '*{line-height:25px;color:#555;font-size:15px;font-family:\'Hiragino Sans GB\',\'Microsoft YaHei\',\'黑体\',Helvetica,Arial,Tahoma,sans-serif;}' +
+    'img{max-width:100%;}' +
+    'img.size-overflowed{box-sizing:border-box;border:2px solid red;-webkit-filter:opacity(.4);filter:opacity(.4);}' +
+    'table{width:100%}',
+  extended_valid_elements: 'a[href|href-id|target=_blank|title]',
+  convert_urls: false,
+};
+
+class App extends Component {
+  handleChange(e) {
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <TapasEditor
+      config={config}
+      onChange={this.handleChange}
+      />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#container'));
